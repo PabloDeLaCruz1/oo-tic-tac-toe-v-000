a92723d328d1ee0dd01c375032234aabcbc06dd6
Pablo De La Cruz <pdelac@ramapo.edu>
2018-04-26 14:49:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,6 +27,10 @@ class TicTacToe
     user_input.to_i - 1
   end 
   
+  def move(index, current_player)
+    @board[index] = current_player
+  end
+
   def valid_move?(position)
     position
   end
