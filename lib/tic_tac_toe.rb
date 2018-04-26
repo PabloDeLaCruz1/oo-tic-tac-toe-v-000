dc3bb9802e3511a467077b7addcb98d887e24920
Pablo De La Cruz <pdelac@ramapo.edu>
2018-04-26 14:42:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,9 +17,13 @@ class TicTacToe
  
 
 
-  def display_board
-    
-  end
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
   
   def valid_move?(position)
     position
